10fbee256482c1fcdfbd23794f436726e895c6ea
zabelin <zabelin_maxim@bk.ru>
2023-01-31 21:45:33 +0700

    FriendDto and PrivateMessagesDto was updated

--- src/main/java/ru/learnup/socialnetwork/dto/PrivateMessagesDto.java
+++ src/main/java/ru/learnup/socialnetwork/dto/PrivateMessagesDto.java
@@ -1,12 +1,16 @@
 package ru.learnup.socialnetwork.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PrivateMessagesDto {
 
     private Long id;
